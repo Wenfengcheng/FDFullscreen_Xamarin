b2ec4dcc648b214c6daf6bee193a0f58ee0bc111
Wenfeng Cheng <WenfengCheng@highviewsoftware.com>
2019-01-28 18:10:38 +0800

    update ApiDefinitions and add comments

--- FDFullscreen/FDFullscreen/ApiDefinition.cs
+++ FDFullscreen/FDFullscreen/ApiDefinition.cs
@@ -5,20 +5,28 @@ using UIKit;
 
 namespace FDFullscreen
 {
-
     // @interface FDFullscreenPopGesture (UINavigationController)
     [Category]
     [BaseType(typeof(UINavigationController))]
     interface UINavigationController_FDFullscreenPopGesture
     {
+        /// <summary>
+        /// The gesture recognizer that actually handles interactive pop.
+        /// </summary>
         // @property (readonly, nonatomic, strong) UIPanGestureRecognizer * fd_fullscreenPopGestureRecognizer;
         [Export("fd_fullscreenPopGestureRecognizer", ArgumentSemantic.Strong)]
         UIPanGestureRecognizer Fd_fullscreenPopGestureRecognizer();
 
+        /// <summary>
+        /// A view controller is able to control navigation bar's appearance by itself,
+        /// </summary>
         // @property (assign, nonatomic) BOOL fd_viewControllerBasedNavigationBarAppearanceEnabled;
         [Export("fd_viewControllerBasedNavigationBarAppearanceEnabled")]
         bool Fd_viewControllerBasedNavigationBarAppearanceEnabled();
 
+        /// <summary>
+        /// A view controller is able to control navigation bar's appearance by itself,
+        /// </summary>
         [Export("setFd_viewControllerBasedNavigationBarAppearanceEnabled:")]
         void SetFd_viewControllerBasedNavigationBarAppearanceEnabled(bool enabled);
     }
@@ -28,20 +36,31 @@ namespace FDFullscreen
     [BaseType(typeof(UIViewController))]
     interface UIViewController_FDFullscreenPopGesture
     {
-            // @property (assign, nonatomic) BOOL fd_interactivePopDisabled;
+        /// <summary>
+        /// Whether the interactive pop gesture is disabled when contained in a navigation stack.
+        /// </summary>
+        // @property (assign, nonatomic) BOOL fd_interactivePopDisabled;
         [Export("fd_interactivePopDisabled")]
         bool Fd_interactivePopDisabled();
 
+        /// <summary>
+        /// Whether the interactive pop gesture is disabled when contained in a navigation stack.
+        /// </summary>
         [Export("setFd_interactivePopDisabled:")]
         void SetFd_interactivePopDisabled(bool disable);
 
+        /// <summary>
+        /// Indicate this view controller prefers its navigation bar hidden or not,
+        /// </summary>
         // @property (assign, nonatomic) BOOL fd_prefersNavigationBarHidden;
         [Export("fd_prefersNavigationBarHidden")]
         bool Fd_prefersNavigationBarHidden();
 
+        /// <summary>
+        /// Indicate this view controller prefers its navigation bar hidden or not,
+        /// </summary>
         [Export("setFd_prefersNavigationBarHidden:")]
         void SetFd_prefersNavigationBarHidden(bool hidden);
     }
-
 }
 
